5b9f0e091f9a0a796419c7acf50a169e213e570d
kyaurix <nabeelul101@gmail.com>
2024-05-21 03:04:58 +0100

    Made a start on the game, made it so the text file dictionary is loaded, read line by line reading each new word identified via a split "," and then adds it to an ArrayList, tested via println statement and works!

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        // Creating an ArrayList to hold all the words in our dictionary which will be added from a text file
+        List<String> words = new ArrayList<>();
 
+        // We use try-catch to stop any file-reading commands from going wrong and crashing the program.
+        try (BufferedReader br = new BufferedReader(new FileReader("C:/Users/Nabee/IdeaProjects/HangmanGame/src/words.txt"))) {
+            String line; // holds the value on the current line of the text file
+            while ((line = br.readLine()) != null) { // while the line is not empty
+                for (String word : line.split(",")) { // Iterates over each word split by "," and assigns it to String 'word'
+                    words.add(word.trim()); // takes our variable word, trims any white/blank space and then adds it to our ArrayList
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
