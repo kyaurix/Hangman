3409045dee0964d9d2307a6fd7cdede4cd11d54b
kyaurix <your.email@example.comnabeelul101@gmail.com>
2024-05-23 02:54:44 +0100

    Added a plan.txt just to type out the plan as to how to create the game (not done to great detail as of yet) and also added more to the code such that now we have a selected word to play the game

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,20 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         // Creating an ArrayList to hold all the words in our dictionary which will be added from a text file
         List<String> words = new ArrayList<>();
+        String pickedWord = wordPicker(words); // calls method which picks a random word
+        //testing
+        System.out.println(pickedWord);
+    }
 
+    public static String wordPicker(List<String> words) {
         // We use try-catch to stop any file-reading commands from going wrong and crashing the program.
-        try (BufferedReader br = new BufferedReader(new FileReader("C:/Users/Nabee/IdeaProjects/HangmanGame/src/words.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("C:/Users/Nabeel/IdeaProjects/Hangman/src/words.txt"))) {
             String line; // holds the value on the current line of the text file
             while ((line = br.readLine()) != null) { // while the line is not empty
                 for (String word : line.split(",")) { // Iterates over each word split by "," and assigns it to String 'word'
@@ -20,5 +26,9 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        Random random = new Random(); //math.random
+        String hangmanWord = words.get(random.nextInt(words.size())); // random word picked from 0-(size of arrayList)
+
+        return hangmanWord; //returns word
     }
 }
